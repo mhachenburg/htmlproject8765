46d0643163db2aa8d2e8784b0aee5a5afaea9a08
mhachenburg <mhachenburg@hotmail.com>
2025-03-17 15:57:53 +0100

    Delete main directory

--- main/js/button.js
+++
@@ -1,6 +0,0 @@
-        function image() {
-            document.getElementById('button-image')
-                .style.display = "block";
-            document.getElementById('button-event')
-                .style.display = "none";
-        }
